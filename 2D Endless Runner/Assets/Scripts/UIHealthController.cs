fda6dcb3a19d6583053bffa9e2529cc3f19c194c
Azhar564 <azharramadhan7777@gmail.com>
2021-10-24 22:33:41 +0700

    Initial Commit

---
+++ 2D Endless Runner/Assets/Scripts/UIHealthController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthController : MonoBehaviour
+{
+    [Header("UI")]
+    public Text healthValue;
+
+    [Header("Health")]
+    public CharacterMoveController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthValue.text = character.health.ToString();
+    }
+}
